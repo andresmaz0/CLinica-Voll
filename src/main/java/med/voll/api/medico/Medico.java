16c50e9f8780ded33d24a1835db43b605532792e
Andres Mazo <amazobo@eafit.edu.co>
2023-09-09 16:33:57 -0500

    Añadiendo anotaciones lombok
    Se añade anotaciones a clase medico y dirección.

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -8,11 +8,20 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import med.voll.api.direccion.Direccion;
 
 
 @Table(name = "medicos")
 @Entity(name = "Medico")
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Medico {
 	
 	@Id
