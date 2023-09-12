79f8c199eccc77cae13f73918720b60c729a1767
Andres Mazo <amazobo@eafit.edu.co>
2023-09-12 16:53:21 -0500

    Creando apartado de get medicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,15 +1,15 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.medico.DatosListadoMedico;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import med.voll.api.medico.DatosRegistroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -23,6 +23,11 @@ public class MedicoController {
 	@PostMapping
 	public void RegistrarMedico(@RequestBody @Valid DatosRegistroMedico datos) {
 		medicoRepository.save(new Medico(datos));
+	}
+
+	@GetMapping
+	public List<DatosListadoMedico> ListadoMedicos(){
+		return medicoRepository.findAll().stream().map(DatosListadoMedico::new).toList();
 	}
 	
 }
