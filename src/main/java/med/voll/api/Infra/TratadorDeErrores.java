0c7ef27136dbdd7b723d09c1556c044874dd2505
Andres Mazo <amazobo@eafit.edu.co>
2023-09-20 22:12:50 -0500

    Tratando error EntityNotFoundException

--- src/main/java/med/voll/api/Infra/TratadorDeErrores.java
+++ src/main/java/med/voll/api/Infra/TratadorDeErrores.java
@@ -1,4 +1,15 @@
 package med.voll.api.Infra;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice //intercepta las llamadas en caso de algun tipo de excepcion
 public class TratadorDeErrores {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarError404(){
+        return ResponseEntity.notFound().build();
+    }
 }
