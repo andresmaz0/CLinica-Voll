1e622865534fcdd71890e863d7201c8beec45f93
Andres Mazo <amazobo@eafit.edu.co>
2023-09-23 16:42:38 -0500

    Creando Seguridad de usuarios

--- src/main/java/med/voll/api/Infra/security/AutenticacionService.java
+++ src/main/java/med/voll/api/Infra/security/AutenticacionService.java
@@ -1,4 +1,19 @@
 package med.voll.api.Infra.security;
 
-public class AutenticacionService {
+import med.voll.api.domain.usuarios.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service //Inidica que es un servicio de la API
+public class AutenticacionService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByUsername(username);
+    }
 }
